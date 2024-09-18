6f79671df9e331518576ad5511a4f700f72a975e
danylo8 <ozai.avatar@gmail.com>
2024-09-18 23:29:03 +0000

    Pending changes exported from your codespace

---
+++ Main.java
@@ -0,0 +1,60 @@
+public class Main
+{
+
+    public static void main(String[] args) 
+    {
+        boolean t=true;
+        boolean f=false;
+
+        System.out.println("Hello. Lets play a math game.");
+        System.out.println("");
+        System.out.println("");
+
+        String equation ="1. 3+2.5=5.5";
+        System.out.println(equation);
+        System.out.println("");
+       
+        System.out.print("The integer here is ");
+        int x=3;
+        System.out.println(x);
+        
+        System.out.print("The doubles here are ");
+        final double y=2.5;
+        double z=5.5;
+        System.out.print(y);
+        System.out.print(" and ");
+        System.out.println(z);
+        
+        System.out.print("The total is ");
+        System.out.println(3+2.5);
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("If I change variable x to 6 the total would change to... ");
+        x=6;
+        System.out.println("");
+        System.out.println(x);
+        System.out.println("");
+        System.out.println("");
+
+        System.out.print("If I change variable x to 186 would the total be ");
+        z=187.5;
+        System.out.print(z);
+        System.out.print(" or ");
+        System.out.print(189.5);
+        System.out.println(" if y stays the same?");
+        System.out.println("");
+
+        System.out.print("The first choice is...");
+        System.out.print(t);
+        System.out.println("");
+        System.out.print("The second choice is...");
+        System.out.println(f);
+        
+        System.out.println("");
+        System.out.println("Congrats if you got it right, if you did not then try again if you want.");
+
+    }
+
+
+}
